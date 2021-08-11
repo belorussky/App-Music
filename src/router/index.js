271a3bf4cc80dfdbf4139c4181a33897a9297831
Raman Belaruski <ramanbelaruski@Ramans-MacBook-Air.local>
2021-08-11 12:30:34 +0200

    add routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,58 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+import store from '@/store';
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'home',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue'),
   },
   {
     path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    name: 'about',
+    component: () => import('../views/About.vue'),
+  },
+  {
+    path: '/manage-music',
+    name: 'manage',
+    meta: {
+      requiresAuth: true,
+    },
+    // alias: '/manage',
+    component: () => import('../views/Manage.vue'),
+    beforeEnter: (to, from, next) => {
+      console.log('Manage Route Guard');
+      next();
+    },
+  },
+  {
+    path: '/manage',
+    redirect: { name: 'manage' },
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkExactActiveClass: 'text-yellow-500',
+});
+
+router.beforeEach((to, from, next) => {
+  // console.log(to.matched);
+  if (!to.matched.some((record) => record.meta.requiresAuth)) {
+    next();
+    return;
+  }
+
+  if (store.state.userLoggedIn) {
+    next();
+  } else {
+    next({ name: 'home' });
+  }
 });
 
 export default router;
